77cc2fc8ebf60c95d345c2ef893eda51e47bad6e
Your Name <peterbubolz@gmail.com>
2024-12-24 09:28:32 -0600

    improve sudo usage

--- install/crafty-controller-install.sh
+++ install/crafty-controller-install.sh
@@ -15,25 +15,30 @@ network_check
 update_os
 
 msg_info "Installing Dependencies"                       # Crafty Controller requires Python 3.9+
-$STD sudo add-apt-repository ppa:deadsnakes/ppa
-$STD sudo apt update
-$STD sudo apt install python3.9 -y
+# $STD sudo add-apt-repository ppa:deadsnakes/ppa
+# $STD sudo apt install python3.9 -y
 # Update and upgrade packages
+# $STD apt-get install -y curl
+$STD apt-get install -y sudo
+# $STD apt-get install -y mc
 $STD apt update && apt upgrade -y
-# $STD apt install python3
+msg_ok "Installed Dependencies"
 # Install Git
+msg_info "Installing Git"
 $STD apt install git -y
-msg_ok "Git Installed"
+msg_ok "Installed Git"
 # Install JDK 21
-$STD apt install openjdk-21-jdk -y
-msg_ok "openjdk Installed"
-msg_info "cloning crafty"
+# msg_info "Installing openjdk"
+# $STD apt install openjdk-21-jdk -y
+# msg_ok "Installed openjdk"
+msg_info "Cloning Crafty"
 # Clone the Crafty installer and run it
 $STD git clone https://gitlab.com/crafty-controller/crafty-installer-4.0.git
+msg_ok "Cloned Crafty"
 msg_info "Installing Crafty"
 
 $STD cd crafty-installer-4.0 && sudo ./install_crafty.sh
-
+msg_ok "Installed Crafty"
 
 msg_info "Creating Service"
 cat <<EOF >/etc/systemd/system/crafty.service
@@ -52,7 +57,7 @@ ExecStart=/opt/crafty/bin/crafty serve
 [Install]
 WantedBy=multi-user.target
 EOF
-systemctl enable -q --now crafty.service
+systemctl enable -q --now crafty
 msg_ok "Created Service"
 
 msg_info "Cleaning up"
